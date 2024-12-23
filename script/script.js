8683b880dfa06e8faad9500b9274b308d8453eee
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2024-12-23 18:14:06 +0900

    수정

--- script/script.js
+++ script/script.js
@@ -3,7 +3,48 @@
 document.addEventListener("DOMContentLoaded", function(e){
     rightMenuSlide();
     mainSlideBanner();
+    mainLogoSlide();
+    serviceSlide();
 })
+
+const serviceSlide = () => {
+    var swiper = new Swiper(".main .specialSection .slideBox .mySwiper", {
+        slidesPerView: 3.2,
+        spaceBetween: 22,
+        loop : true, 
+        loopAdditionalSlides: 5,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            900: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            650: {
+                slidesPerView: 1.1,
+                spaceBetween: 10,
+            },
+        },
+    });
+}
+
+const mainLogoSlide = () => {
+    var swiper = new Swiper(".main .logoSection .logoBox .mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 0, // important !!
+            disableOnInteraction: false,
+        },
+        speed: 5000,
+        loop : true,
+        loopAdditionalSlides: 1,
+        slidesPerView: 8,
+        allowTouchMove: false,
+    });
+}
 
 const mainSlideBanner = () => {
     var swiper = new Swiper(".main .bannerSlide .mySwiper", {
