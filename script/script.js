7b4904a9de7f77555aacad9269a47b09811972c5
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2024-12-23 15:28:17 +0900

    메인 퍼블리싱중

--- script/script.js
+++ script/script.js
@@ -1,7 +1,36 @@
 
 
 document.addEventListener("DOMContentLoaded", function(e){
-
+    rightMenuSlide();
+    mainSlideBanner();
 })
 
+const mainSlideBanner = () => {
+    var swiper = new Swiper(".main .bannerSlide .mySwiper", {
+        loop : true, 
+        effect: "fade",
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".main .bannerSlide .slideArti .halfBox .contentBox .btnWrapDiv .btnList .nextBtn",
+            prevEl: ".main .bannerSlide .slideArti .halfBox .contentBox .btnWrapDiv .btnList .prevBtn",
+        },
+    });
+}
 
+const rightMenuSlide = () => {
+    $('#header_24 .sideMenuBtn').click(function(){
+        $('.menuCloseBg').addClass('show');
+        $('.rightMenuSection').addClass('show');
+    })
+    $('.menuCloseBg').click(function(){
+        $('.menuCloseBg').removeClass('show');
+        $('.rightMenuSection').removeClass('show');
+    })
+    $('.rightMenuSection .closeBtnDiv .closeBtn').click(function(){
+        $('.menuCloseBg').removeClass('show');
+        $('.rightMenuSection').removeClass('show');
+    })
+}
